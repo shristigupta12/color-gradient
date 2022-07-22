b31532e1c0586d16f7baafebcf6b8a6c670360bc
Shristi <shristiguptawork@gmail.com>
2022-07-22 15:44:22 +0530

    add color feature added

--- 03ColorGradient/script.js
+++ 03ColorGradient/script.js
@@ -1,24 +1,74 @@
-let colorBtn1 = document.querySelector("#color-btn1");
-let colorBtn2 = document.querySelector("#color-btn2");
-let input1 = document.querySelector("#input1");
-let input2 = document.querySelector("#input2");
-let apply = document.querySelector(".apply-btn");
-let gradient = document.querySelector(".gradient-wrapper");
+let grandient = document.querySelector(".gradient-wrapper");
+let parent = document.querySelector(".color-picker-wrapper");
+let colorCount = 1;
+let colors = [];
+// let gradientValue = "linear-gradient(to right"
+
+function elements(){
+    //color
+    let color = document.createElement("div");
+    color.classList.add("colors");
+    //div
+    let div = document.createElement("div");
+    div.classList.add("colors");
+    let divText = document.createTextNode("Color "+colorCount);
+    div.appendChild(divText);
+    //colorInp
+    let colorInp = document.createElement("input");
+    colorInp.setAttribute('type','color');
+    colorInp.classList.add("color-btn");
+    let colorInpId = `${colorCount}`;
+    colorInp.setAttribute('id', colorInpId);
+    //textInp
+    let textInp = document.createElement("input");
+    textInp.setAttribute('type','text');
+    textInp.classList.add("color-input");
+    let textInpId = `textInp${colorCount}`;
+    textInp.setAttribute('id', textInpId);
+
+    // append
+    color.append(div);
+    color.append(colorInp);
+    color.append(textInp);
+
+    //selecting color event           // value of text and color needs to be changed in colors 
+    colorInp.addEventListener("mouseout", ()=>{
+        textInp.value = colorInp.value;  
+    });
+
+    // gradientValue = gradientValue.concat(","+colorInp.value);       //issue
+
+    //push color in colors array
+    colors.push(color);
+
+    colorCount++;
+    return color;
+}
+
+parent.append(elements());
+parent.append(elements());
+
+let addColorBtn = document.querySelector(".add-more-color-wrapper");
+addColorBtn.addEventListener("click",()=>{
+    parent.append(elements());
+});
+let rmvColorBtn = document.querySelector(".remove-color-wrapper");
+rmvColorBtn.addEventListener("click",()=>{
+    parent.pop();                       // not working
+});
+
 let applyBtn = document.querySelector(".apply-btn");
-
-colorBtn1.addEventListener("mouseout", ()=>{
-    input1.value = colorBtn1.value;
-
-});
-colorBtn2.addEventListener("mouseout", ()=>{
-    input2.value = colorBtn2.value;
-});
-apply.addEventListener("click", ()=>{
-    gradient.style.background = "linear-gradient(to right,"+input1.value+","+input2.value+")";
-})
 applyBtn.addEventListener("mousedown", ()=>{
     applyBtn.style.padding = "6px 14px";
 })
 applyBtn.addEventListener("mouseup", ()=>{
     applyBtn.style.padding = "8px 16px";
 })
+// applyBtn.addEventListener("click", ()=>{
+//     grandient.style.background = gradientValue+")";
+//     console.log(gradientValue+")");
+// })
+
+
+
+
